0a92f3dd934cb14fb114cc10845189da7db1a383
nazmi <nazmiegret@gmail.com>
2026-05-23 22:27:18 +0300

    feat: add Linux-specific process group management for deployment tasks and include dashboard binary

--- internal/dashboard/gitdeploy_linux.go
+++ internal/dashboard/gitdeploy_linux.go
@@ -13,20 +13,6 @@ func setProcAttributes(cmd *exec.Cmd) {
 		cmd.SysProcAttr = &syscall.SysProcAttr{}
 	}
 	cmd.SysProcAttr.Setpgid = true
-	// Enforce memory limit of 2GB (Soft and Hard RSS/AS limits)
-	const memLimit = 2 * 1024 * 1024 * 1024 // 2 GB
-	cmd.SysProcAttr.Rlimits = []syscall.Rlimit{
-		{
-			Type: syscall.RLIMIT_AS,
-			Cur:  memLimit,
-			Max:  memLimit,
-		},
-		{
-			Type: syscall.RLIMIT_DATA,
-			Cur:  memLimit,
-			Max:  memLimit,
-		},
-	}
 }
 
 func killProcessGroup(cmd *exec.Cmd) {
